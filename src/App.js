6e0100ef96738841d37e5d1f6aa5c7049e123b64
IvanJMartinezS <ivanmartinezwork@gmail.com>
2023-02-10 11:10:48 -0400

    Add Components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Menu from './Menu';
+import List from './List';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      books:[
+        {id:0, rating: 4, title: 'Harry Potter y el cáliz de fuego', image: 'libro01.jpg'},
+        {id:1, rating: 3, title: 'The shining', image: 'libro02.jpg'},
+        {id:2, rating: 5, title: 'Código Da Vinci', image: 'libro03.jpg'},
+        {id:3, rating: 5, title: 'El principito', image: 'libro04.jpg'},
+        {id:4, rating: 5, title: 'Sobrenatural', image: 'libro05.jpg'}
+      ],
+      copyBooks: []
+    };
+  }
+
+
+  render(){
+    return (
+      <div className="app">
+        <Menu title="AmazonJR" />
+        <List className="list" items={this.state.books}/>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Search.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Search extends React.Component{
+
+
+    render(){
+        return(
+            <input type="text" />
+        );
+    }
+}
+
+export default Search;
