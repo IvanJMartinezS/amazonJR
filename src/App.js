83abc8b1ddd868266c02b4fd364a629ee5c0a85e
IvanJMartinezS <ivanmartinezwork@gmail.com>
2023-02-10 18:15:43 -0400

    add functions and events - Create, Update, Read and Delete props

--- src/App.js
+++ src/App.js
@@ -19,20 +19,66 @@ class App extends React.Component{
     };
   }
 
+  componentDidMount(){
+    this.initBooks();
+  }
+
+  initBooks(){
+    this.setState((state,props) => ({
+      copyBooks: [... state.books]
+    }));
+  }
+
   onAdd = (item) => {
     var temp = [...this.state.books];
     const id = temp[temp.length-1].id + 1;
     item['id'] = id;
     temp.push(item);
     this.setState({books: [...temp]});
+    this.initBooks();
   }
 
+  onSearch = (query) => {
+    if(query === ''){
+      this.setState({copyBooks: [... this.state.books]});
+    }else{
+      const temp = [...this.state.books];
+      var res = [];
+      temp.forEach(item =>{
+        if(item.title.toLowerCase().indexOf(query) > -1){
+          res.push(item);
+        }
+      });
+    
+      this.setState({copyBooks: [...res]});
+    }
+  }
+
+  onUpdateRating = (item) => {
+    var temp = [... this.state.books];
+    const index = temp.findIndex(x => x.id === item.id);
+
+    temp[index].title = item.title;
+    temp[index].image = item.image;
+    temp[index].rating = item.rating;
+
+    this.setState({books: [...temp]});
+    this.initBooks();
+
+  }
+
+  onRemove = (id) => {
+    var temp = [...this.state.books];
+    const res = temp.filter(item => item.id != id);
+    this.setState({books: [...res]});
+    this.initBooks();
+  }
 
   render(){
     return (
       <div className="app">
-        <Menu title="AmazonJR" onadd={this.onAdd}/>
-        <List className="list" items={this.state.books}/>
+        <Menu title="AmazonJR" onsearch={this.onSearch} onadd={this.onAdd}/>
+        <List className="list" items={this.state.copyBooks} onupdaterating={this.onUpdateRating} onremove={this.onRemove}/>
       </div>
     );
   }
--- src/Search.js
+++ src/Search.js
@@ -2,10 +2,19 @@ import React from 'react';
 
 class Search extends React.Component{
 
+    constructor(props){
+        super(props);
+
+    }
+
+    onChangeEvent = (e) => {
+        const query = e.target.value.toString().toLowerCase();
+        this.props.onsearch(query);
+    }
 
     render(){
         return(
-            <input type="text" />
+            <input type="text" onChange={this.onChangeEvent} />
         );
     }
 }
